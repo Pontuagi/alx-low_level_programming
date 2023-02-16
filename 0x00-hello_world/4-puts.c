4a4823a70ef8891d4265d15a91ad269a4d8794be
Pontuagi <benepontuagi@gmail.com>
2023-02-16 13:51:38 -0800

    print with printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/*Print a string*/
+
 int main(void)
 {
 	char word[] = "\"Programming is like building a multilingual puzzle";
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printf prints string
+ * Return : always 0 (Success)
+ */
+
+int main(void)
+{
+
+/* print a string using print */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+/* end of main*/
