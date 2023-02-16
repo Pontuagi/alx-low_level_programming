10b08fb367db23055295169e953430a36dad9b96
Pontuagi <benepontuagi@gmail.com>
2023-02-16 15:13:59 -0800

    6 correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function for our program
+ *main - this is the main function of our program
  *description - print size of variables
- *return - return 0
+ *return - return 0 is successful
  */
 
 int main(void)
 {
-	int intype;
+	int inttype;
 	char chartype;
 	long int longinttype;
 	long long int longlonginttype;
 	float floattype;
 
-	printf("Size of a char: %zu bytes/n", sizeof(chartype));
-	printf("Size of an int: %zu bytes/n", sizeof(inttype));
-	printf("Size of a long int: %zu bytes/n", sizeof(longinttype));
-	printf("Size of a long long int: %zu bytes/n", sizeof(longlonginttype));
-	printf("Size of float: %zu bytes/n", sizeof(floattype));
+	printf("Size of a char: %ld bytes\n", sizeof(chartype));
+	printf("Size of an int: %ld bytes\n", sizeof(inttype));
+	printf("Size of a long int: %ld bytes\n", sizeof(longinttype));
+	printf("Size of a long long int: %ld bytes\n", sizeof(longlonginttype));
+	printf("Size of float: %ld bytes\n", sizeof(floattype));
 
 	return (0);
 }
