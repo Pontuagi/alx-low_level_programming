46f434c99474beb56097b7fb7b2fc94489b92a93
Pontuagi <benepontuagi@gmail.com>
2023-02-17 14:48:45 -0800

    print some of alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - main function
+  *Description: print lowercase alphabet except q and e
+  *Return: return 0
+  */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != e && ch != q)
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
