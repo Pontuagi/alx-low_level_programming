7801e96fc9ab950cd973ce15caa335ec6ebb0bb6
Pontuagi <benepontuagi@gmail.com>
2023-02-17 15:26:08 -0800

    Numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - the main function
+  *Description: print numbers from 0 to 9
+  *Return: return 0
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
