6562dd8c26635f8e900516b77ba2bdcc463c43d5
Pontuagi <benepontuagi@gmail.com>
2023-02-17 18:13:27 -0800

    3 combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - main function
+  *Description: combine 3 numbers and print the least combinations
+  *Return: 0
+  */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10 ; k++)
+			{
+				if (100 * i + 10 * J + k < 100 * k + 10 * i + j && 100 * j + 10 * i + k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (100 * i + 10 * j + k != 789)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
