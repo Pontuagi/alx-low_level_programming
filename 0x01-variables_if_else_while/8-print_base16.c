7bde42c9721738419000be1a8d3405b3ab6e6eba
Pontuagi <benepontuagi@gmail.com>
2023-02-17 16:08:48 -0800

    Base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  *main - main function
+  *Description: print characters of base 16
+  *Return: return 0
+  */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch < 'g'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
