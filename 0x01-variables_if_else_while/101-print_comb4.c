d7bf481028b2447e87c0ba93abbc508f2d8b4801
Pontuagi <benepontuagi@gmail.com>
2023-02-20 13:16:40 -0800

    101 correction 002

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if (!(k == 9 || j == 8 || i == 7))
+					if (!(k == 9 && j == 8 && i == 7))
 					{
 					putchar(',');
 					putchar(' ');
