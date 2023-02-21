d6f8f13c6754bdb892dfb5f70ac63bb1309730c7
Pontuagi <benepontuagi@gmail.com>
2023-02-21 15:43:05 -0800

    print sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_sign - check if a number is positive or negative
+  * @n: number to iterate
+  * Desprition: Using if to check the value of a number against 0
+  * Return: return int
+  */
+
+int print_sign(int n)
+{
+	int f;
+
+	if (n > 0)
+	{
+		f = 1;
+		putchar('+');
+	}
+	else if (n == 0)
+	{
+		f = 0;
+		putchar('0');
+	}
+	else
+	{
+		f = -1;
+		putchar('-')
+	}
+	return (f);
+}
