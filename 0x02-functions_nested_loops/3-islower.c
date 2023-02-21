c39bb361247ef36180a4aa1b392a69428d704095
Pontuagi <benepontuagi@gmail.com>
2023-02-21 14:25:08 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _islower - function to check if a letter is lowercase
+  *Return: return 1 if c is lowercase otherwise 0
+  */
+
+int _islower(int)
+{
+	int c, i;
+
+	i = islower(c);
+	return (i);
+}
