3305cfc4004da36ac67a5f4e5d208ae59e3e1496
Pontuagi <benepontuagi@gmail.com>
2023-02-21 13:46:29 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * main - entry to our code
+  * Return: Return 0 always
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
