50078996ab92668ffb9f84a37f351279706d0ec3
Pontuagi <benepontuagi@gmail.com>
2023-02-21 15:58:32 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _abs - entry to our function
+  * Return: 1 or 0
+  * @int: value to check absolute value of
+  */
+
+int _abs(int)
+{
+	int r;
+	int j = abs(r);
+
+	return (j);
+}
