2423e40296b160d3e3ce0bf277f14e2c122f9ab6
Pontuagi <benepontuagi@gmail.com>
2023-02-21 18:16:36 -0800

    times table 003

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,17 +17,22 @@ void times_table(void)
 			if (n < 10)
 			{
 				_putchar(n + '0');
+				if (j < 9)
+				{
+				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				}
 			}
 			else
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-			}
-			if (j < 9)
-			{
-			_putchar(',');
-			_putchar(' ');
+				if (j < 9)
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
