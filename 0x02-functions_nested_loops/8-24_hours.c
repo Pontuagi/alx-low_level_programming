391795a9b66e8eeec4b26c7c2d1daf26bec552e9
Pontuagi <benepontuagi@gmail.com>
2023-02-21 17:12:10 -0800

    minutes of day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+  * jack_bauer - print every minute of the day
+  * Return: void
+  */
+
+void jack_bauer(void)
+{
+	int j, k, l, m;
+
+	for (j = 0; j < 3; j++)
+	{
+		for (k = 0; k < 4; k++)
+		{
+			for (l = 0; l < 6; l++)
+			{
+				for (m = 0; m < 10; m++)
+				{
+					_putchar(j);
+					_putchar(k);
+					_putchar(':');
+					_putchar(l);
+					_putchar(m);
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
