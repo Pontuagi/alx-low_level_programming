db1eb4d6b59b3ad74f65de1a7335c6c23e545fa4
Pontuagi <benepontuagi@gmail.com>
2023-02-21 16:19:56 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_last_digit - function to print last digit of a number
+  * @j: digit to check
+  * Return: last digit
+  */
+
+int print_last_digit(int j)
+{
+	int lstdgt = j % 10;
+
+	return (lstdgt);
+}
