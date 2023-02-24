5fe7ae73fb1edcc1b1cabf12be59b3e534be1de2
Pontuagi <benepontuagi@gmail.com>
2023-02-23 16:45:09 -0800

    print nlines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_line - print a line
+  * @n: variable
+  */
+
+void print_line(int n)
+{
+	int m;
+
+	for (m = 0; m <= n; m++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
