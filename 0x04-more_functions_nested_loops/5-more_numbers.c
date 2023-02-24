2ad97f5db5ea41d8f0146d662c52523e0a065898
Pontuagi <benepontuagi@gmail.com>
2023-02-23 16:36:29 -0800

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * more_numbers - function to print numbers
+  */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+			_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
