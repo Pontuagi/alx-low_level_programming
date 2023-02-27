57fd6c87a458838f8da507e1638ee7e11a40e749
Pontuagi <benepontuagi@gmail.com>
2023-02-27 04:43:21 -0800

    stringlenth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strlen - function to return the length of a string
+  * @s: string whose legth to check
+  * Return: return length of the string
+  */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = strlen(s);
+	return (n);
+}
