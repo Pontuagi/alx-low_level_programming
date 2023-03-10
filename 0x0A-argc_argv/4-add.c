3131578d8cbfaed5414c6963f54795dcf2049b3a
Pontuagi <benepontuagi@gmail.com>
2023-03-10 14:24:35 -0800

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
   * main - main function
@@ -15,7 +16,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) > 0)
+		if ((atoi(argv[i]) % 10) > 0 && (atoi(argv[i]) % 10) < 10)
 		{
 		sum = sum + atoi(argv[i]);
 		}
