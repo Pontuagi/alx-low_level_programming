bded7ad60c87074829f168ed9de7635cd709de61
Pontuagi <benepontuagi@gmail.com>
2023-03-21 14:06:33 -0700

    struct init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 /**
   * init_dog - initialize dog
   * @d: initialize dog
@@ -13,7 +11,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
   * Return: d
   */
 
-struct dog;
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
