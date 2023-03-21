4df37c47d420677722eaf24049645f2fbc215bcb
Pontuagi <benepontuagi@gmail.com>
2023-03-21 06:34:16 -0700

    malloc array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,16 +12,27 @@
 int **alloc_grid(int width, int height)
 {
 	int **ar;
-	int i;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	ar = malloc(width * sizeof(int *));
-	for (i = 0; i < width; i++)
-		ar[i] = malloc(height * sizeof(int));
+	ar = malloc(height * sizeof(int *));
 	if (ar == NULL)
 		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		ar[i] = malloc(width * sizeof(int));
+		if (ar[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(ar[j]);
+			free(ar);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			ar[i][j] = 0;
+	}
 	return (ar);
 }
