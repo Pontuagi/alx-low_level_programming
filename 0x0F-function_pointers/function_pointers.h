5634067e47e1429fe61cd03a687d24ede972e323
Pontuagi <benepontuagi@gmail.com>
2023-03-22 14:37:38 -0700

    print name 01

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,8 @@
 #define _FUNCTION_POINTERS_H_
 
 #include <stddef.h>
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
