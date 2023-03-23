24dbf37c5199a1d1c147fc502f2792770952de01
Pontuagi <benepontuagi@gmail.com>
2023-03-23 15:04:42 -0700

    print numbers 01

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(pnt, int));
-			printf("%s", separator);
+			if (i != n - 1)
+				printf("%s", separator);
 		}
 	}
 	printf("\n");
