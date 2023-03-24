09272389a5cd289aa33ae7310a0ea234887a106b
Pontuagi <benepontuagi@gmail.com>
2023-03-23 17:49:24 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,65 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  * print_all - print all types of texts
+  * @format: format of the text
+  */
+
+void print_all(const char * const format, ...)
+{
+	int i;
+	va_list pnt;
+
+	va_start(pnt, format);
+	i = 0;
+
+	if (format[i])
+	{
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				{
+					char c = va_arg(pnt, int);
+
+					printf("%c", c);
+					break;
+				}
+			case 'i':
+				{
+					int j = va_arg(pnt, int);
+
+					printf("%d", j);
+					break;
+				}
+			case 'f':
+				{
+					int f = va_arg(pnt, double);
+
+					printf("%d", f);
+					break;
+				}
+			case 's':
+				{
+					char *st = va_arg(pnt, char *);
+
+					printf("%s", st);
+					break;
+				}
+			default:
+					break;
+		}
+		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] ==
+					'i' || format[i] == 'f' || format[i] ==
+					's'))
+			printf(", ");
+		i++;
+	}
+	}
+	printf("\n");
+
+	va_end(pnt);
+}
