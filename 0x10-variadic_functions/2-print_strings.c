769f4a6a1226c841536e529726fdf940cc9c9e65
Pontuagi <benepontuagi@gmail.com>
2023-03-23 18:02:24 -0700

    print string 03

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		j = va_arg(pnt, char*);
 		if (j == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 		{
 			printf("%s", j);
