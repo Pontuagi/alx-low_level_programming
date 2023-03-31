93be7c616ae2c7ecd6af97aacaa04ae9d0a1c7df
Pontuagi <benepontuagi@gmail.com>
2023-03-31 11:42:23 -0700

    print before executing main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+  * print_before_main - print a string before main is executed.
+  */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
