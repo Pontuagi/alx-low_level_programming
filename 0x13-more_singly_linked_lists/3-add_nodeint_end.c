c6d529a2a8f3c4d16330df09a08332857ae28a2d
Pontuagi <benepontuagi@gmail.com>
2023-04-04 12:00:27 -0700

    add element at tail of list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+  * add_nodeint_end - and element at the tail of linked list
+  * @head: pointer to head of list
+  * @n: data to add to end of list
+  *
+  * Return: return pointer to last element or NULL
+  */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));
+	listint_t *lastnode;
+
+	newnode->n = n;
+	newnode->next = NULL;
+
+	if (head == NULL || newnode == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		lastnode = *head;
+
+		while (lastnode->next != NULL)
+			lastnode = lastnode->next;
+		lastnode->next = newnode;
+	}
+
+	return (newnode);
+}
