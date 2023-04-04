187a842e191a6c7332bf9e1fc773969f6b33d811
Pontuagi <benepontuagi@gmail.com>
2023-04-04 11:34:11 -0700

    add node at head 02

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode = (listint_t *)malloc(sizeof(listint_t));
 
-	if (head == NULL)
+	if (head == NULL || newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = *head;
