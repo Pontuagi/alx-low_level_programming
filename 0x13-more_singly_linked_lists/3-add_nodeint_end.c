2b129b00dcb0a3cd47044047b969c42c744dfde7
Pontuagi <benepontuagi@gmail.com>
2023-04-04 14:07:37 -0700

    add element at tail of list 02

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,11 +13,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *newnode = malloc(sizeof(listint_t));
 	listint_t *lastnode;
 
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
 
-	if (newnode == NULL)
-		return (NULL);
 	if (*head == NULL)
 	{
 		*head = newnode;
