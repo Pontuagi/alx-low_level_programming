782ba09a4a45be5e046f59640d02ba524603dc87
Pontuagi <benepontuagi@gmail.com>
2023-04-04 19:27:21 -0700

    print list 01

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,28 +9,41 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
+	size_t count = 0, loop = 0;
 	const listint_t *sin = head, *two = head;
 
-	while (two != NULL && two->next != NULL)
+	while (two && two->next)
 	{
-		printf("[0x%p] %d\n", (void *)sin, sin->n);
-		count++;
 		sin = sin->next;
 		two = two->next->next;
+		count++;
 
 		if (sin == two)
 		{
-			printf("[0x%p] %d\n", (void *)sin, sin->n);
-			count++;
-			exit(98);
+			loop = 1;
+			break;
 		}
 	}
-
-	if (two != NULL)
+	if (loop)
 	{
-		printf("[0x%p] %d\n", (void *)two, two->n);
-		count++;
+		sin = head;
+
+		while (sin != two)
+		{
+		printf("0x[%p] %d\n", (void *)sin, sin->n);
+		sin = sin->next;
+		two = two->next;
+		}
+		printf("0x[%p} %d\n", (void *)sin, sin->n);
+	}
+	else
+	{
+		while (head)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			count++;
+		}
 	}
 
 	return (count);
