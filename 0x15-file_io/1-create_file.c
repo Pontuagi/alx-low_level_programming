8dd2de76377d2e6a922ca4ccf302a3b9d0d92363
Pontuagi <benepontuagi@gmail.com>
2023-04-13 00:38:59 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fg;
 	ssize_t write_bytes;
-	ssize content_len;
+	ssize_t content_len;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		content_len = strlen(text_content);
-		write_bytes = write(fg, text_content, strlen(text_content));
-		if (write_bytes != content_len)
+		write_bytes = write(fg, text_content, content_len);
+		if (write_bytes == -1)
 		{
 			close(fg);
 			return (-1);
