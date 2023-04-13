d998370305dd91f00ab088183b045375ae03bc67
Pontuagi <benepontuagi@gmail.com>
2023-04-13 06:44:25 -0700

    copy content from one file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+  * print_error - print error message on the POSIX standard error and exit
+  * @text: the message to output.
+  * @name: name of the file
+  * @number: exit code.
+  */
+
+void print_error(char *text, char *name, int number)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", text, name);
+	exit(number);
+}
+
+/**
+  * main - program to copy content from one file to another
+  * @files: number of arguments
+  * @v: pointer to arguments
+  *
+  * Return: return 0 if successful.
+  */
+
+int main(int files, char *v[])
+{
+	int fa_from, fb_to;
+	char buffer[BUFFER_SIZE];
+	char *file_from;
+	ssize_t fa_read, fb_write;
+
+	if (files != 3)
+		print_error("Usage: cp file_from file_to", NULL, 97);
+
+	/* open fa_from */
+	file_from = v[1];
+	fa_from = open(file_from, O_RDONLY);
+	if (fa_from == -1)
+		print_error("Error: Can't read from file", v[1], 98);
+
+	if (buffer == NULL)
+		return (-1);
+
+	/* open fa_to */
+	fb_to = open(v[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
+			S_IRGRP | S_IWGRP | S_IROTH);
+	if (fb_to == -1)
+		print_error("Error: Can't write to", v[2], 99);
+
+	/* read from file fa_from and write to file fb_to */
+	while ((fa_read = read(fa_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		fb_write = write(fb_to, buffer, BUFFER_SIZE);
+		if (fb_write == -1)
+			print_error("Error: Can't write to", v[2], 99);
+	}
+
+	if (fa_read == -1)
+		print_error("Error: Can't read from file", v[1], 98);
+	if (close(fa_from) == -1)
+		print_error("Error: Can't close fd FD_VALUE", NULL, 100);
+	if (close(fb_to) == -1)
+		print_error("Error: Can't close fd FD_VALUE", NULL, 100);
+
+	return (0);
+}
