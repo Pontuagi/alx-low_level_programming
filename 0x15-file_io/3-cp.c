a744d61f9aed5225b3a0dfe7100ea3c148d01eff
Pontuagi <benepontuagi@gmail.com>
2023-04-13 07:02:00 -0700

    copy content from one file to another 01

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,9 @@
 
 void print_error(char *text, char *name, int number)
 {
-	dprintf(STDERR_FILENO, "%s %s\n", text, name);
+	if (name == NULL)
+		dprintf(STDERR_FILENO, "%s\n", text);
+	dprintf(STDERR_FILENO, "%s%s\n", text, name);
 	exit(number);
 }
 
@@ -27,7 +29,7 @@ int main(int files, char *v[])
 {
 	int fa_from, fb_to;
 	char buffer[BUFFER_SIZE];
-	char *file_from;
+	char *file_from, *file_to;
 	ssize_t fa_read, fb_write;
 
 	if (files != 3)
@@ -35,6 +37,7 @@ int main(int files, char *v[])
 
 	/* open fa_from */
 	file_from = v[1];
+	file_to = v[2];
 	fa_from = open(file_from, O_RDONLY);
 	if (fa_from == -1)
 		print_error("Error: Can't read from file", v[1], 98);
@@ -43,7 +46,7 @@ int main(int files, char *v[])
 		return (-1);
 
 	/* open fa_to */
-	fb_to = open(v[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
+	fb_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
 			S_IRGRP | S_IWGRP | S_IROTH);
 	if (fb_to == -1)
 		print_error("Error: Can't write to", v[2], 99);
