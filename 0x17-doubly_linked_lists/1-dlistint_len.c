81ae8fd621b45ac5c06ea5009b3bc11af084255d
Pontuagi <benepontuagi@gmail.com>
2023-05-18 14:12:25 -0700

    count elements of a list 01

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * dlistint_t - function to count the elements of a linked list
+  * dlistint_len - function to count the elements of a linked list
   * @h: head pointer
   *
   * Return: return count elements in the list
@@ -10,7 +10,7 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t count = 0;
-	
+
 	if (h == NULL)
 		return (0);
 	while (current != NULL)
