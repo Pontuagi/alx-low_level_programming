e03447ad856f8b1b8b043ad194294f1cd619b425
Pontuagi <benepontuagi@gmail.com>
2023-05-18 16:58:43 -0700

    add node at an index 03

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current, *new_node;
 	unsigned int count = 0;
 
-	if (h == NULL)
+	if (*h == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -23,8 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 	if (idx == 0)
 	{
-		if (*h != NULL)
-			(*h)->prev = new_node;
+		(*h)->prev = new_node;
 		new_node->next = *h;
 		*h = new_node;
 		return (new_node);
@@ -35,13 +34,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		count++;
 	}
-	if (count != idx)
+	if (count != idx && current == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	if (current != NULL)
-		current->prev->next = new_node;
+	current->prev->next = new_node;
 	new_node->prev = current->prev;
 	new_node->next = current;
 	current->prev = new_node;
