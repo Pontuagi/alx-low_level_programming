f6e6ae929eb931e2a8795db75669525ab424442d
Pontuagi <benepontuagi@gmail.com>
2023-05-22 07:03:21 -0700

    dynamic library called from c

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include "main.h"                                                               
+                                                                                
+/**                                                                             
+  * add - add two numbers                                                       
+  * @a: number 1                                                                
+  * @b: second number                                                           
+  *                                                                             
+  * Return: Return sum                                                          
+  */                                                                            
+int mul(int a, int b)
+{
+	return(a * b);
+}  
