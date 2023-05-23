652e9b31bc907a0658db7ceaef535ed575e5458a
Pontuagi <benepontuagi@gmail.com>
2023-05-22 22:36:05 -0700

    script to create dynamic libraries

---
+++ 0x18-dynamic_libraries/main2.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN2_H_
+#define _MAIN2_H_
+
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
