2c3abced3cb9870cb4f7444dffd52c94d79c1f4a
Pontuagi <benepontuagi@gmail.com>
2023-07-27 05:33:38 +0300

    technical interniew preparation correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,19 +11,19 @@ def island_perimeter(grid):
     The function takes one argument (grid)
     """
     rows = len(grid)
-    colmns= len(grid[0])
+    colmns = len(grid[0])
 
     perimeter = 0
 
     for row in range(rows):
         for colm in range(colmns):
-            if grid[row][colm] ==1:
+            if grid[row][colm] == 1:
                 perimeter += 4
 
-                #check for adjacent grids
-                if row > 0 and grid[row -1][colm] == 1:
+                # check for adjacent grids
+                if row > 0 and grid[row - 1][colm] == 1:
                     perimeter -= 1
-                if row < rows -1 and grid[row + 1][colm] == 1:
+                if row < rows - 1 and grid[row + 1][colm] == 1:
                     perimeter -= 1
                 if colm > 0 and grid[row][colm - 1] == 1:
                     perimeter -= 1
