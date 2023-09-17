76d66185fb42d1c1f703bd148b760af574b8f4cd
Pontuagi <benepontuagi@gmail.com>
2023-09-17 19:48:40 +0300

    dog correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,21 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog;
+
+/**
+  * struct dog - short structure
+  * @name: first element
+  * @age: second element
+  * @owner: third element
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -13,11 +27,5 @@ void print_dog(struct dog *d);
   * @owner: third element
   */
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
 
 #endif /* ifndef _DOG_H_ */
